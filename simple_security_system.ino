207c9bad9fca30faf4d1110e36d620c73d4a028d
Malith Sankalpa Udawaththa <75824407+sankalpa-udawaththa@users.noreply.github.com>
2021-11-23 08:53:36 +0530

    This is simple security system using Arduino

---
+++ simple_security_system.ino
@@ -0,0 +1,24 @@
+#define trig 2
+#define echo 3
+void setup() {
+pinMode(trig,OUTPUT);
+pinMode(echo,INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite(trig,HIGH);
+delayMicroseconds(10);
+digitalWrite(trig,LOW);
+
+long t = pulseIn(echo,HIGH);
+long inches = t/74/2;
+long Cm = t/29/2;
+
+Serial.print(inches);
+Serial.print("in");
+Serial.print(Cm);
+Serial.println("cm");
+}
